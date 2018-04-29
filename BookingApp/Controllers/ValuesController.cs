d62267257b24a9bb7b641b5ced1be375d26b5382
bohdanka194 <bohdanka194@gmail.com>
2018-04-29 21:42:59 +0300

    First Commit

---
+++ BookingApp/Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BookingApp.Models;
+using System.Data;
+
+namespace BookingApp.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        BookContext db = new BookContext();
+
+        [HttpGet]
+        public IEnumerable<Book> GetBooks()
+        {
+            return db.Books;
+        }
+
+        public Book GetBook(int id)
+        {
+            Book book = db.Books.Find(id);
+            return book;
+        }
+
+        [HttpPost]
+        public void CreateBook([FromBody]Book book)
+        {
+            db.Books.Add(book);
+            db.SaveChanges();
+        }
+
+        [HttpPut]
+        public void EditBook(int id, [FromBody] Book book)
+        {
+            if (id == book.Id)
+            {
+                db.Entry(book).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            Book book = db.Books.Find(id);
+            if (book != null)
+            {
+                db.Books.Remove(book);
+                db.SaveChanges();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        // now we realized controller on CRUD model
+    }
+}
---
+++ BookingApp/Models/BookDbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BookingApp.Models
+{
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Origin", Author = "Dan Brown", Year = 2017 });
+            db.Books.Add(new Book { Name = "Kobzar", Author = "Taras Shevchenko", Year = 1860 });
+            db.Books.Add(new Book { Name = "CLR via C#", Author = "Jeffrey Richter", Year = 2013});
+
+            base.Seed(db);
+        }
+    }
+}
